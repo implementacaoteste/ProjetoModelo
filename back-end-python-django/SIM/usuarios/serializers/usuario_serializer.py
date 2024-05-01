5b53d672bdeb78f64c0b67faa72c5909ce0121ab
Erisvaldo Carvalho <erisvaldodesenvolvedor@gmail.com>
2024-05-01 00:14:56 -0300

    Arquivos organizados, pronto para colocar no container docker e mudar o banco para mysql

--- back-end-python-django/SIM/usuarios/serializers/usuario_serializer.py
+++ back-end-python-django/SIM/usuarios/serializers/usuario_serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from ..models import Usuario
+from ..models.usuario import Usuario
 
 class UsuarioSerializer(serializers.ModelSerializer):
     class Meta:
--- back-end-python-django/SIM/usuarios/urls.py
+++ back-end-python-django/SIM/usuarios/urls.py
@@ -2,12 +2,11 @@
 
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from usuarios.views import UsuarioViewSet
+from .views.usuario_view import UsuarioViewSet
 
 router = DefaultRouter()
-router.register(r'', UsuarioViewSet)
+router.register(r'', UsuarioViewSet, basename = 'usuario')
 
 urlpatterns = [
     path('', include(router.urls)),
 ]
-
