d7e0fd3ce2d9b4f0cedd1ba202fcfba81d4c4167
Erisvaldo Carvalho <erisvaldodesenvolvedor@gmail.com>
2024-05-03 10:05:06 -0300

    Exclusão do primeiro teste de back django

--- back-end-python-django2/core/cadastro/serializers/usuario_serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from ..models.usuario import Usuario
-
-class UsuarioSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Usuario
-        fields = '__all__'
